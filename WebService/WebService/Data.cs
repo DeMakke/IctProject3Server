5c82d9c37b879ed9132b6c712098958e93972ba0
Dries <vandeurzen.dries@gmail.com>
2016-10-08 18:48:14 +0200

    verloren items terug toegevoegd

---
+++ WebService/WebService/Data.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService
+{
+    public class Data
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("name")]
+        public int name { get; set; }
+        [JsonProperty("base64")]
+        public int base64 { get; set; }
+    }
+}
---
+++ WebService/WebService/JsonCode.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService
+{
+    public class JsonCode
+    {
+        Data data = new Data();
+
+        public Data JsonDeCoding(string json)
+        {
+            Data data = JsonConvert.DeserializeObject<Data>(json);
+            return data;
+        }
+
+        public string JsonCoding(Data data)
+        {
+            string json = JsonConvert.SerializeObject(data);
+            return json;
+        }
+    }
+}
