944909951bd5d3426b4df7ed91aba6b001fdc7e8
Dries <vandeurzen.dries@gmail.com>
2016-10-09 15:59:59 +0200

    Basic configuration of wcf webservice

---
+++ WebService/WebService/IWebService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWebService" in both code and config file together.
+    [ServiceContract]
+    public interface IWebService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "Json/Default")]
+        string Default(Stream Data);
+    }
+}
---
+++ WebService/WebService/WebService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WebService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select WebService.svc or WebService.svc.cs at the Solution Explorer and start debugging.
+
+    public class WebService : IWebService
+    {
+        public string Default(Stream data)
+        {
+            StreamReader reader = new StreamReader(data);
+            String JSONData = reader.ReadToEnd();
+
+            return "Positive response";
+        }
+    }
+}
